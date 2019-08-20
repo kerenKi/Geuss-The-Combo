bf367b9f50f20292da28463ac4aa7308d94399f2
kerenKi <kkinberg13@gmail.com>
2019-08-20 18:54:46 +0200

    Higtlight selected color

---
+++ src/app/models/Colors.ts
@@ -0,0 +1,4 @@
+export class ColorOption {
+  color:string;
+  isSelected:boolean;
+}
--- src/components/color-options/color-options.component.ts
+++ src/components/color-options/color-options.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColorOption } from '../../app/models/Colors'
 
 @Component({
   selector: 'app-color-options',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorOptionsComponent implements OnInit {
 
-  circles: string[] = ['red','yellow','green','blue','orange','purple']
+  circles: ColorOption[] = [
+    {
+      color:'red',
+      isSelected: false
+    },
+    {
+      color:'yellow',
+      isSelected: false
+    },  {
+      color:'green',
+      isSelected: false
+    },  {
+      color:'blue',
+      isSelected: true
+    },  {
+      color:'orange',
+      isSelected: false
+    },  {
+      color:'purple',
+      isSelected: false
+    }
+  ]
 
   ngOnInit() {
   }
