980134b3892d7a43a5636ff212653b658a9317d9
kerenKi <kkinberg13@gmail.com>
2019-08-20 09:55:54 +0200

    Created  'game', 'about', 'instructions' components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { AboutComponent } from './about/about.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    AboutComponent,
+    InstructionsComponent
   ],
   imports: [
     BrowserModule,
