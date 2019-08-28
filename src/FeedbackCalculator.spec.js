ddffc9ee30e2f2c8025312716cd2568aa64d30e2
kerenKi <kkinberg13@gmail.com>
2019-08-28 12:18:01 +0200

    Test for FeedbackCalculator

--- src/FeedbackCalculator.spec.js
+++ src/FeedbackCalculator.spec.js
@@ -6,11 +6,20 @@ var winCode = [Colors_1.Color.Red, Colors_1.Color.Red, Colors_1.Color.Purple, Co
 var guessCode = [Colors_1.Color.Purple, Colors_1.Color.Red, Colors_1.Color.Yellow, Colors_1.Color.Purple];
 var expectedFeedback = [Colors_1.Color.Hit, Colors_1.Color.CorrectColor, Colors_1.Color.Miss, Colors_1.Color.Miss];
 var feedback = FeedbackCalculator_1.calculateFeedback(winCode, guessCode);
+function isEqual(feedbackArray, expectedFeedbackArray) {
+    if (feedbackArray.length !== expectedFeedbackArray.length) {
+        console.log('length is not equal');
+        return false;
+    }
+    var valuesEqual = feedbackArray.filter(function (color, index) { return color === expectedFeedbackArray[index]; });
+    if (valuesEqual.length === 4) {
+        console.log('arrays are equal');
+        return true;
+    }
+    return false;
+}
 console.log('expected:', expectedFeedback);
 console.log('recieved:', feedback);
-if (feedback !== expectedFeedback) {
-    console.log('Bad Feedback!');
-}
-else {
-    console.log('Good job!');
-}
+test(' feedback and expected feedback Arrays should be equal', function () {
+    expect(isEqual(feedback, expectedFeedback)).toBe(true);
+});
--- src/FeedbackCalculator.spec.ts
+++ src/FeedbackCalculator.spec.ts
@@ -6,12 +6,26 @@ const guessCode = [Color.Purple, Color.Red, Color.Yellow, Color.Purple]
 const expectedFeedback = [Color.Hit, Color.CorrectColor, Color.Miss, Color.Miss]
 
 const feedback = calculateFeedback(winCode, guessCode)
+function isEqual(feedbackArray,expectedFeedbackArray)  {
+  if(feedbackArray.length !== expectedFeedbackArray.length){
+    console.log('length is not equal')
+    return false
+  }
+
+const valuesEqual = feedbackArray.filter((color, index) => color === expectedFeedbackArray[index])
+if (valuesEqual.length === 4) {
+  console.log('arrays are equal')
+  return true
+  }
+
+  return false
+} 
 
 console.log('expected:', expectedFeedback)
 console.log('recieved:', feedback)
 
-if (feedback !== expectedFeedback){
-  console.log('Bad Feedback!')
-} else {
-  console.log('Good job!')
-}
+test(' feedback and expected feedback Arrays should be equal', ()=> {
+  expect(isEqual(feedback, expectedFeedback)).toBe(true)
+})
+
+
